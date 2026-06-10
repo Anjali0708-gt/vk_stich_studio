877f858bbdb83090d42046537fd21dda6070f6b8
Anjali0708-gt <anj13062004@gmail.com>
2026-06-10 21:54:20 +0530

    login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import react from 'react';
+import './home.css';
 function Home()
 {
   return(
     <>
-      <h1>Welcome to Our Website</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    <h1 className="container-h1">Welcome to VK Stich Studio</h1>
+    <div className="container">
+      
+      <div className="herosection">
+        
+        <h2>Crafted for Your Story. Tailored for Your Legacy.</h2>
+        <p>The suit you wear to your first interview, your wedding day, or a special celebration becomes part of your story. At VK Stitch Studio, we craft custom garments designed not just to fit your body, but to stay with your memories for years to come.</p>
+      </div>
+
+      <div className="side-img">
+        <img src="src/assets/bacgroudhome img.webp" alt="Background Image" />
+      </div>
+    </div>
+
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,38 @@
 import react from 'react';
-import { Link } from 'react-router-dom';    
+import axios from 'axios';
+import { Link } from 'react-router-dom';  
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';  
 function Login()
 {
+     const [username, setUsername] = useState('');
+     const[password,setpassword]=useState('');
+     const handleLogin = async () => {
+        try {
+            const req = await axios.post('http://localhost:3000/api/login', { username, password });
+            console.log(req.data);
+            toast.success('Login successful!');
+        } catch (error) {
+            console.error('Error logging in:', error);
+            toast.error('Login failed!');
+        }
+     }
+
+
     return(
         
     <>
-        <h1>Login Page</h1>
+        <div className="logincontainer">
+            <h1>Login Page</h1>
+            <div className="login-form">
+                <form action="" method="post" onSubmit={handleLogin}>
+                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+
+        </div>
     </>
     
     )
